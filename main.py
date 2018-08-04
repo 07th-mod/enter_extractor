3af19badb9020972a9d4779976ee931e99a11234
Daniel Wong <drojfjord@gmail.com>
2018-08-04 10:53:48 +1000

    Add main to do actual converseion

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 #external dependencies:
 # - Bitstring (Run pip install bitstring)
 # - PyQt5 (Run pip install pyqt5 . Ignore any IDE warnings - this is because of pyqt5's wierd way of installing)
+from pic import convert_pic
+from bup import convert_bup
+
+# convert_pic(r'C:\tempextract\extract_old\picture\a\bg_black.pic', r'C:\tempextract\output\hikari.png')
+convert_bup(r'C:\tempextract\Sui\aks2.bup', r'C:\tempextract\output')
+# convert_pic(r'C:\tempextract\Sui\ga1.pic', r'C:\tempextract\output\asdf.png')
