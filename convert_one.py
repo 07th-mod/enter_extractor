c3c73c38a3128c0722b94d2550e94b28d1a15f7f
Daniel Wong <drojfjord@gmail.com>
2018-11-26 19:13:52 +1100

    restore command line interface

--- convert_one.py
+++ convert_one.py
@@ -5,24 +5,24 @@ import conf
 from pic import convert_pic
 from bup import convert_bup
 
-# input = sys.argv[1]
-# output = sys.argv[2]
-#
-# _, input_extension = os.path.split(input)
-#
-# if input_extension == '.pic':
-#   convert_pic(input, output)
-# else:
-#   convert_bup(input, output)
+input = sys.argv[1]
+output = sys.argv[2]
 
+_, input_extension = os.path.split(input)
 
-input = r'C:\temp\buptest\kei1.bup'
-input = r'C:\temp\buptest\ri1.bup'
-input = r'c:\temp\buptest\ha1.bup'
-input = r'c:\temp\buptest\hnit1a.bup'
-input = r'c:\temp\buptest\ta1.bup'
-input = r'c:\temp\buptest\miyo1.bup'
-input = r'c:\temp\buptest\miyuki.bup'
+if input_extension == '.pic':
+  convert_pic(input, output)
+else:
+  convert_bup(input, output)
+
+#
+# input = r'C:\temp\buptest\kei1.bup'
+# input = r'C:\temp\buptest\ri1.bup'
+# input = r'c:\temp\buptest\ha1.bup'
+# input = r'c:\temp\buptest\hnit1a.bup'
+# input = r'c:\temp\buptest\ta1.bup'
+# input = r'c:\temp\buptest\miyo1.bup'
+# input = r'c:\temp\buptest\miyuki.bup'
 
 
 # input = r'c:\temp\buptest\renasen1.bup'
@@ -30,7 +30,7 @@ input = r'c:\temp\buptest\miyuki.bup'
 # input = r'C:\temp\buptest\sui_aks2.bup'
 # conf.SWITCH = False
 
-output = r'C:\temp\buptest\out'
+# output = r'C:\temp\buptest\out'
 
 # convert_pic(sys.argv[1], sys.argv[2])
-convert_bup(input, output)
+# convert_bup(input, output)
