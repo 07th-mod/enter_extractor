ec8a2456b44000a56b54c77eed0c4d5028affd97
TellowKrinkle <tellowkrinkle@gmail.com>
2021-01-31 23:28:08 -0600

    Don't complain about writing transparent pixels over transparent pixels

--- src/PartsBupOutputter.cpp
+++ src/PartsBupOutputter.cpp
@@ -11,19 +11,24 @@ class PartsBupOutputter: public BupOutputter {
 	std::string withMouthName;
 	Point facePos = {0, 0};
 	Point mouthPos = {0, 0};
+	Image base;
+	Image withFace;
 	Image tmp;
 
-	enum class ColorType {
-		AllOpaque,
-		HasPartiallyTransparent,
-		HasFullyTransparent,
+	enum class Blend {
+		/// Images can be composited using normal alpha blending
+		AlphaBlend,
+		/// Images need a blend function that copies pixels if the src alpha is != 0
+		CustomBlend,
+		/// No blend function will properly recreate the image :(
+		NotPossible,
 	};
 
-	ColorType faceColor, mouthColor;
+	Blend faceBlend, mouthBlend;
 
 	struct ImageEntry {
 		std::vector<std::pair<Point, std::string>> parts;
-		ColorType color = ColorType::AllOpaque;
+		Blend color = Blend::AlphaBlend;
 	};
 
 	std::vector<std::pair<std::string, std::vector<ImageEntry>>> expressions;
@@ -35,9 +40,10 @@ public:
 		img.writePNG(partsPath/(name + ".png"));
 		this->basePath = std::move(basePath);
 		baseName = std::move(name);
+		base = std::move(img);
 	}
 
-	ColorType prepareImageInTmp(const Image& img, const std::vector<MaskRect>& mask) {
+	Blend prepareImageInTmp(const Image& img, Point pos, const std::vector<MaskRect>& mask, const Image& target) {
 		tmp.fastResize(img.size);
 		std::fill(tmp.colorData.begin(), tmp.colorData.end(), Color());
 		bool hasPartiallyTransparent = false;
@@ -47,6 +53,9 @@ public:
 			for (int y = section.y1; y < section.y2; y++) {
 				for (int x = section.x1; x < section.x2; x++) {
 					Color px = img.pixel(x, y);
+					if (target.pixel(pos.x + x, pos.y + y).a == 0) {
+						continue;
+					}
 					if (px.a == 0) {
 						hasFullyTransparent = true;
 					}
@@ -56,22 +65,24 @@ public:
 				}
 			}
 		}
-		return hasFullyTransparent ? ColorType::HasFullyTransparent
-		     : hasPartiallyTransparent ? ColorType::HasPartiallyTransparent
-		     : ColorType::AllOpaque;
+		return hasFullyTransparent ? Blend::NotPossible
+		     : hasPartiallyTransparent ? Blend::CustomBlend
+		     : Blend::AlphaBlend;
 	}
 
 	void newFace(const Image& img, Point pos, const std::string &name, const std::vector<MaskRect>& mask) override {
+		withFace = base;
 		withMouthName = "";
-		mouthColor = ColorType::AllOpaque;
+		mouthBlend = Blend::AlphaBlend;
 		facePos = pos;
 		if (img.empty()) {
 			withFaceName = "";
-			faceColor = ColorType::AllOpaque;
+			faceBlend = Blend::AlphaBlend;
 		}
 		else {
+			img.drawOnto(withFace, pos, mask);
 			withFaceName = baseName + "_" + name;
-			faceColor = prepareImageInTmp(img, mask);
+			faceBlend = prepareImageInTmp(img, pos, mask, base);
 			tmp.writePNG(partsPath/(withFaceName + ".png"));
 		}
 		expressions.push_back({name, {}});
@@ -80,7 +91,7 @@ public:
 	void newMouth(const Image& img, Point pos, int num, const std::vector<MaskRect>& mask) override {
 		withMouthName = (withFaceName.empty() ? baseName : withFaceName) + "_" + std::to_string(num);
 		mouthPos = pos;
-		mouthColor = prepareImageInTmp(img, mask);
+		mouthBlend = prepareImageInTmp(img, pos, mask, withFace);
 		tmp.writePNG(partsPath/(withMouthName + ".png"));
 	}
 
@@ -88,10 +99,10 @@ public:
 		expressions.back().second.emplace_back();
 		ImageEntry& cur = expressions.back().second.back();
 
-		auto addColor = [&](ColorType type){
-			if (cur.color == ColorType::AllOpaque) {
+		auto addColor = [&](Blend type){
+			if (cur.color == Blend::AlphaBlend) {
 				cur.color = type;
-			} else if (type == ColorType::HasFullyTransparent) {
+			} else if (type == Blend::NotPossible) {
 				cur.color = type;
 			}
 		};
@@ -102,13 +113,13 @@ public:
 		cur.parts.push_back({{0, 0}, path(baseName)});
 		if (!withFaceName.empty()) {
 			cur.parts.push_back({facePos, path(withFaceName)});
-			addColor(faceColor);
+			addColor(faceBlend);
 		}
 		if (!withMouthName.empty()) {
 			cur.parts.push_back({mouthPos, path(withMouthName)});
-			addColor(mouthColor);
+			addColor(mouthBlend);
 		}
-		if (cur.color == ColorType::HasFullyTransparent) {
+		if (cur.color == Blend::NotPossible) {
 			std::cerr << "Warning: " << withFaceName << " has fully transparent overridden pixels, it may not be properly renderable" << std::endl;
 		}
 	}
@@ -135,12 +146,12 @@ public:
 			out << "\t\"" << exp.first << "\": [" << std::endl;
 			for (const auto& img : exp.second) {
 				out << "\t\t{" << std::endl;
-				out << "\t\t\t\"color_type\": \"";
+				out << "\t\t\t\"blend\": \"";
 				switch (img.color) {
-#define OUT(x) case ColorType::x: out << #x; break;
-						OUT(AllOpaque)
-						OUT(HasPartiallyTransparent)
-						OUT(HasFullyTransparent)
+#define OUT(x) case Blend::x: out << #x; break;
+						OUT(AlphaBlend)
+						OUT(CustomBlend)
+						OUT(NotPossible)
 #undef OUT
 				}
 				out << "\"," << std::endl;
